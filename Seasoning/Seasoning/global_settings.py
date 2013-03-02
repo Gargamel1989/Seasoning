d5c1a239ed1793f92431e4453e929429e19c7780
Gargamel1989 <joeper_100@hotmail.com>
2013-03-02 01:27:12 +0100

    static serve fix

--- Seasoning/Seasoning/global_settings.py
+++ Seasoning/Seasoning/global_settings.py
@@ -46,7 +46,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(SETTINGS_DIR), 'static'),
+    os.path.join(os.path.dirname(SETTINGS_DIR), 'Seasoning/static'),
 )
 
 # List of finder classes that know how to find static files in
