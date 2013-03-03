7f82faeb7a16afb10811bd131c34d65e1aeff03c
Gargamel1989 <joeper_100@hotmail.com>
2013-03-03 17:26:19 +0100

    Added static pages

--- Seasoning/Seasoning/urls.py
+++ Seasoning/Seasoning/urls.py
@@ -3,5 +3,11 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     # General Pages
     url(r'^$', 'Seasoning.views.home', name='home'),
+    url(r'^contact/$', 'Seasoning.views.contact'),
+    url(r'^motifs/$', 'Seasoning.views.motifs'),
+    url(r'^privacypolicy/$', 'Seasoning.views.privacypolicy'),
+    url(r'^sitemap/$', 'Seasoning.views.sitemap'),
+    url(r'^support/$', 'Seasoning.views.support'),
+    url(r'^terms/$', 'Seasoning.views.terms'),
 
 )
--- Seasoning/Seasoning/views.py
+++ Seasoning/Seasoning/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'homepage.html')
+    return render(request, 'seasoning/homepage.html')
+
+def contact(request):
+    return render(request, 'seasoning/contact.html')
+
+def motifs(request):
+    return render(request, 'seasoning/motifs.html')
+
+def privacypolicy(request):
+    return render(request, 'seasoning/privacypolicy.html')
+
+def sitemap(request):
+    return render(request, 'seasoning/sitemap.html')
+
+def support(request):
+    return render(request, 'seasoning/support.html')
+
+def terms(request):
+    return render(request, 'seasoning/terms.html')
