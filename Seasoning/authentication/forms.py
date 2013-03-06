9e24e1174b58883858f67ed9e3d7cd13270ac768
Gargamel1989 <joeper_100@hotmail.com>
2013-03-06 21:01:51 +0100

    Internalized registration app and updated for use in django 1.5

--- Seasoning/authentication/forms.py
+++ Seasoning/authentication/forms.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.forms import AuthenticationForm,PasswordResetForm, SetPasswordForm
+from django.contrib.auth.forms import AuthenticationForm
 from django import forms
 from django.contrib.auth import authenticate, REDIRECT_FIELD_NAME
 from django.contrib.auth.models import User
 from captcha.fields import ReCaptchaField
-from django.forms.fields import BooleanField
 from django.utils.safestring import mark_safe
 from django.conf import settings
 from registration.forms import RegistrationFormUniqueEmail
@@ -45,19 +44,66 @@ class EmailAuthenticationForm(AuthenticationForm):
         self.check_for_test_cookie()
         return self.cleaned_data
     
+attrs_dict = {'class': 'required'}
+
 class EmailUserCreationForm(RegistrationFormUniqueEmail):
     """
-    Subclass of ``RegistrationFormUniqueEmail`` which adds a required checkbox
-    for agreeing to a site's Terms of Service and a recaptcha.
+    Form for registering a new user account.
+    
+    Validates that the requested username is not already in use, and
+    requires the password to be entered twice to catch typos.
+    
+    Subclasses should feel free to add any additional validation they
+    need, but should avoid defining a ``save()`` method -- the actual
+    saving of collected user data is delegated to the active
+    registration backend.
     
     """
-    tos = forms.BooleanField(widget=forms.CheckboxInput(),
+    username = forms.RegexField(regex=r'^[\w.@+-]+$',
+                                max_length=30,
+                                widget=forms.TextInput(attrs=attrs_dict),
+                                label=_("Username"),
+                                error_messages={'invalid': _("This value may contain only letters, numbers and @/./+/-/_ characters.")})
+    email = forms.EmailField(widget=forms.TextInput(attrs=dict(attrs_dict,
+                                                               maxlength=75)),
+                             label=_("E-mail"))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs=attrs_dict, render_value=False),
+                                label=_("Password"))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs=attrs_dict, render_value=False),
+                                label=_("Password (again)"))
+    
+    tos = forms.BooleanField(widget=forms.CheckboxInput(attrs=attrs_dict),
                              label=_(u'I have read and agree to the Terms of Service'),
                              error_messages={'required': _("You must agree to the terms to register")})
                              
     captcha = ReCaptchaField(attrs={'theme': 'clean',
                                     'tabindex': 5},
                              error_messages = {'required': _("You must enter the correct ReCaptcha characters")})
+    
+    def clean_username(self):
+        """
+        Validate that the username is alphanumeric and is not already
+        in use.
+        
+        """
+        existing = User.objects.filter(username__iexact=self.cleaned_data['username'])
+        if existing.exists():
+            raise forms.ValidationError(_("A user with that username already exists."))
+        else:
+            return self.cleaned_data['username']
+
+    def clean(self):
+        """
+        Verifiy that the values entered into the two password fields
+        match. Note that an error here will end up in
+        ``non_field_errors()`` because it doesn't apply to a single
+        field.
+        
+        """
+        if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
+            if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+                raise forms.ValidationError(_("The two password fields didn't match."))
+        return self.cleaned_data
 
     
 class ResendActivationEmailForm(forms.Form):
