c69df7cffbc6d7511283d1cc5d16c57c9dbbe1f4
Gargamel1989 <joeper_100@hotmail.com>
2013-03-12 17:24:09 +0100

    Made ingredient models unmanaged and created SQL script to initialize database instead

--- Seasoning/ingredients/models.py
+++ Seasoning/ingredients/models.py
@@ -14,6 +14,10 @@ class Ingredient(models.Model):
     dependent on time and has no special attributes
     '''
     
+    class Meta:
+        managed = False
+        db_table = 'ingredient'
+    
     CATEGORIES = ((u'GO',u'Groenten'),
                      (u'FR',u'Fruit'),
                      (u'KN',u'Knollen'),
@@ -30,41 +34,57 @@ class Ingredient(models.Model):
                  (u'VG',u'Vegetarisch'),
                  (u'NV',u'Niet-Vegetarisch'))
     
-    type = models.CharField(max_length=10)
+    id = models.IntegerField(primary_key=True)
+    type = models.CharField(max_length=10L)
     
-    category = models.CharField(max_length=2, choices=CATEGORIES)
-    veganism = models.CharField(max_length=2, choices=VEGANISMS)
+    category = models.CharField(max_length=2L, choices=CATEGORIES)
+    veganism = models.CharField(max_length=2L, choices=VEGANISMS)
     
-    description = models.TextField()
-    conservation_tip = models.TextField()
-    preperation_tip = models.TextField()
-    properties = models.TextField()
-    source = models.TextField()
+    description = models.TextField(blank=True)
+    conservation_tip = models.TextField(blank=True)
+    preparation_tip = models.TextField(blank=True)
+    properties = models.TextField(blank=True)
+    source = models.TextField(blank=True)
     
     base_footprint = models.FloatField()
     
     image = ProcessedImageField(format='PNG', upload_to=get_image_filename, default='images/ingredients/no_image.png')
-    
     accepted = models.BooleanField(default=False)
 
 class Synonym(models.Model):
     
-    name = models.CharField(max_length=50, unique=True)
-    plural_name = models.CharField(max_length=50, unique=True)
+    class Meta:
+        managed = False
+        db_table = 'synonym'
     
-    ingredient = models.ForeignKey(Ingredient)
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50L, unique=True)
+    plural_name = models.CharField(max_length=50L, blank=True)
+    
+    ingredient = models.ForeignKey(Ingredient, null=True, db_column='ingredient', blank=True)
+    
     
 class Unit(models.Model):
     
-    name = models.CharField(max_length=30, unique=True)
-    short_name = models.CharField(max_length=10)
+    class Meta:
+        managed = False
+        db_table = 'unit'
+        
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30L, unique=True)
+    short_name = models.CharField(max_length=10L)
     
 class CanUseUnit(models.Model):
-    ingredient = models.ForeignKey(Ingredient)
-    unit = models.ForeignKey(Unit)
+    
+    class Meta:
+        managed = False
+        db_table = 'canuseunit'
+        
+    id = models.IntegerField(primary_key=True)
+    ingredient = models.ForeignKey('Ingredient', db_column='ingredient')
+    unit = models.ForeignKey('Unit', db_column='unit')
     
     conversion_factor = models.FloatField()
-
 
 
 class VegetalIngredient(models.Model):
@@ -76,56 +96,80 @@ class VegetalIngredient(models.Model):
     and is a certain distance from Belgium.
     '''
     
-    ingredient = models.ForeignKey(Ingredient, primary_key=True)
-    preservability = models.PositiveIntegerField()
-    preservation_footprint = models.FloatField()
+    class Meta:
+        managed = False
+        db_table = 'vegetalingredient'
+    
+    ingredient = models.ForeignKey(Ingredient, primary_key=True, db_column='ingredient')
+    preservability = models.IntegerField()
+    preservation_footprint = models.FloatField(null=True)
 
 class Country(models.Model):
     '''
     This class represent countries. Each country is a certain distance away from Belgium
     '''
     
-    name = models.CharField(max_length=50)
-    distance = models.PositiveIntegerField()
+    class Meta:
+        managed = False
+        db_table = 'country'
+    
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50L)
+    distance = models.IntegerField()
     
 class TransportMethod(models.Model):
     '''
     This class represents a transport method. A transport method has a mean carbon emission
     per km
     '''
-    name = models.CharField(max_length=30)
+    
+    class Meta:
+        managed = False
+        db_table = 'transportmethod'
+        
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20L)
     emission_per_km = models.FloatField()
-
+    
 class AvailableInCountry(models.Model):
     
-    ingredient = models.ForeignKey(VegetalIngredient)
-    country = models.ForeignKey(Country)
-    transport_method = models.ForeignKey(TransportMethod)
-    production_type  = models.CharField(max_length=10)
+    class Meta:
+        managed = False
+        db_table = 'availableincountry'
+    
+    id = models.IntegerField(primary_key=True)
+    ingredient = models.ForeignKey('Vegetalingredient', db_column='ingredient')
+    country = models.ForeignKey('Country', db_column='country')
+    transport_method = models.ForeignKey('Transportmethod', db_column='transport_method')
+    
+    production_type  = models.CharField(max_length=10, blank=True)
     
     date_from = models.DateField()
     date_until = models.DateField()
     
 
 
-class FishIngredient(models.Model):
-    '''
-    This class represents Fish ingredients.
-    '''
-    
-    ingredient = models.ForeignKey(Ingredient, primary_key=True)
-    
 class Sea(models.Model):
     
-    name = models.CharField(max_length=30)
-    distance = models.PositiveIntegerField()
-
+    class Meta:
+        managed = False
+        db_table = 'Sea'
+    
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50L)
+    distance = models.IntegerField()
+    
 class AvailableInSea(models.Model):
     
-    ingredient = models.ForeignKey(VegetalIngredient)
-    sea = models.ForeignKey(Sea)
-    transport_method = models.ForeignKey(TransportMethod)
+    class Meta:
+        managed = False
+        db_table = 'availableinsea'
+    
+    id = models.IntegerField(primary_key=True)
+    ingredient = models.ForeignKey('Ingredient', db_column='ingredient')
+    sea = models.ForeignKey('Sea', db_column='sea')
+    transport_method = models.ForeignKey('Transportmethod', db_column='transport_method')
     
     date_from = models.DateField()
     date_until = models.DateField()
-
+    
