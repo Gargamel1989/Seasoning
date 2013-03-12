afad0a8996394292f3366b7630b506a09bd355b8
Gargamel1989 <joeper_100@hotmail.com>
2013-03-12 14:59:01 +0100

    Made 'view ingredient' page

--- Seasoning/ingredients/urls.py
+++ Seasoning/ingredients/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'Seasoning.ingredients.views.list_ingredients', name='ingredients_list'),
-    url(r'^(\d*)/$', 'Seasoning.ingredients.views.view_ingredient', name='ingredient'),
+    url(r'^(\d*)/$', 'ingredients.views.view_ingredient', name='ingredient'),
 )
--- Seasoning/ingredients/views.py
+++ Seasoning/ingredients/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+
+from django.shortcuts import render
+from ingredients.models import Ingredient
+
+def view_ingredient(request, ingredient_id):
+    ingredient = Ingredient.objects.get(pk=ingredient_id)
+
+    return render(request, 'ingredients/view_ingredient.html', {'ingredient': ingredient})# Create your views here.
