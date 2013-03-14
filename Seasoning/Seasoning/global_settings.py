be898e47f07b840f6537b69f3294888b177e6abb
Gargamel1989 <joeper_100@hotmail.com>
2013-03-14 21:04:15 +0100

    Fixed deprecated warning

--- Seasoning/Seasoning/global_settings.py
+++ Seasoning/Seasoning/global_settings.py
@@ -112,9 +112,15 @@ AUTH_USER_MODEL = 'authentication.User'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         },
         'console': {
