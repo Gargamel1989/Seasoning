841dab062f4c5fc843ba94b7a7b41f4cc99d591e
Gargamel1989 <joeper_100@hotmail.com>
2013-03-17 09:05:32 +0100

    Each ingredient has one standard name

--- Seasoning/ingredients/models.py
+++ Seasoning/ingredients/models.py
@@ -58,6 +58,8 @@ class Ingredient(models.Model):
                  (u'NV',u'Niet-Vegetarisch'))
     
     id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50L, unique=True)
+    plural_name = models.CharField(max_length=50L, blank=True)
     type = models.CharField(max_length=10L)
     
     category = models.CharField(max_length=2L, choices=CATEGORIES)
