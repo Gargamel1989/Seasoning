48642b1194ede3851028d4ad787dcfcfcccb2906
Gargamel1989 <joeper_100@hotmail.com>
2013-03-17 13:42:59 +0100

    Show recipe page

--- Seasoning/recipes/views.py
+++ Seasoning/recipes/views.py
@@ -17,4 +17,6 @@ def search_recipes(request):
     return render(request, 'recipes/search_recipes.html', {'recipes': recipes})
 
 def view_recipe(request, recipe_id):
-    return render(request, 'recipes/view_recipe.html')
+    recipe = Recipe.objects.get_everything(recipe_id=recipe_id)
+    
+    return render(request, 'recipes/view_recipe.html', {'recipe': recipe})
