22e1c03f5df6f2dbe4a9bd68663d2d4e490cc946
Gargamel1989 <joeper_100@hotmail.com>
2013-03-17 13:41:46 +0100

    Enhanced recipe models

--- Seasoning/recipes/models.py
+++ Seasoning/recipes/models.py
@@ -5,38 +5,85 @@ from imagekit.models.fields import ProcessedImageField, ImageSpecField
 from imagekit.processors.resize import ResizeToFit, AddBorder
 import ingredients
 import itertools
+from ingredients.models import AvailableInCountry, AvailableInSea, CanUseUnit
+import datetime
+from django.db.models import Q
 
 
 class RecipeManager(models.Manager):
     
     def get_everything(self, recipe_id):
-        '''
-        This method returns the recipe and all models related to it. They related models are
-        put in the following fields if they exist:
-            - Cuisine:           cuisine
-            - UsesIngredient:    ingredients
-            - UsesRecipe:        ingredients
-        '''
         recipe = self.select_related().get(pk=recipe_id)
         recipe.ingredients = self.get_ingredients(recipe)
         return recipe
     
+    # Get everything used by the recipe
+    # Returns a list of UsesIngredient and UsesRecipe objects!
     def get_ingredients(self, recipe):
         ingredients = self.get_ingredient_ingredients(recipe)
         recipes = self.get_recipe_ingredients(recipe)
         ingredients_list = list(itertools.chain(ingredients, recipes))
-        print ingredients_list
-        # TODO: sort ingredients_list by group, then by category and then alphabettically
+        ingredients_list = sorted(ingredients_list, key=lambda ingredient: ingredient.group + ingredient.ingredient.name)
         return ingredients_list
     
+    # Get all the recipes used by the recipe
+    # Returns a list of UsesRecipe objects!
     def get_recipe_ingredients(self, recipe):
         recipe_ingredients = UsesRecipe.objects.select_related('ingredient').filter(recipe=recipe)
         for recipe_ingredient in recipe_ingredients:
             recipe_ingredient.ingredients = self.get_ingredients(recipe_ingredient)
         return recipe_ingredients
     
+    # Get all the ingredients used by the recipe
+    # Returns a list of UsesIngredient objects!
     def get_ingredient_ingredients(self, recipe):
-        return UsesIngredient.objects.select_related('ingredient', 'unit').filter(recipe=recipe)
+        uses = UsesIngredient.objects.select_related('ingredient__primary_unit__unit', 'unit').filter(recipe=recipe)
+        ings = {}
+        # Build a dictionary of ingredients, mapping ids to the ingredient objects
+        for use in uses:
+            ings[use.ingredient.pk] = use.ingredient, use
+            
+        # Query database for available_in objects of the found ingredients that are available now
+        current_month = datetime.date.today().month
+        date_filter = (Q(date_from__lte=datetime.date(2000, current_month, 1)) & Q(date_until__gte=datetime.date(2000, current_month, 1)))
+        avails_in_c = AvailableInCountry.objects.select_related('country', 'transport_method').filter(date_filter, ingredient__in=ings.keys())
+        avails_in_s = AvailableInSea.objects.select_related('sea', 'transport_method').filter(ingredient__in=ings.keys())
+        
+        # Map available_in objects to their ingredients and calculate the total_footprint of each available_in
+        for avail in avails_in_c:
+            ingredient = ings[avail.ingredient_id][0]
+            avail.total_footprint = ingredient.base_footprint + \
+                                    avail.extra_production_footprint + \
+                                    avail.country.distance * avail.transport_method.emission_per_km
+            if hasattr(ingredient, 'available_ins'):
+                ingredient.available_ins.append(avail)
+            else:
+                ingredient.available_ins = [avail]
+        
+        for avail in avails_in_s:
+            ingredient = ings[avail.ingredient_id][0]
+            avail.total_footprint = ingredient.base_footprint + \
+                                    avail.extra_production_footprint + \
+                                    avail.sea.distance * avail.transport_method.emission_per_km
+            if hasattr(ingredient, 'available_ins'):
+                ingredient.available_ins.append(avail)
+            else:
+                ingredient.available_ins = [avail]
+        
+        # Query the can_use_unit informations of all ingredients
+        units = CanUseUnit.objects.raw('SELECT * FROM canuseunit JOIN usesingredient ON canuseunit.ingredient = usesingredient.ingredient WHERE usesingredient.recipe = %s', [recipe.id])
+        
+        # Each unit in this list corresponds to an ingredient
+        for unit in units:
+            ingredient, use = ings[unit.ingredient_id]
+            if ingredient.type == 'Veggie' or ingredient.type == 'Fish':
+                ingredient.available_ins = sorted(ingredient.available_ins, key=lambda avail: avail.total_footprint)
+                unweighted_footprint = ingredient.available_ins[0].total_footprint
+            else:
+                unweighted_footprint = ingredient.base_footprint
+            ingredient.total_footprint = unweighted_footprint * use.amount * unit.conversion_factor
+            
+        return uses
     
     
 
@@ -56,6 +103,8 @@ class Cuisine(models.Model):
         return self.name
 
 class Recipe(models.Model):
+    
+    objects = RecipeManager()
     
     class Meta:
         db_table = 'recipe'
@@ -81,7 +130,8 @@ class Recipe(models.Model):
     portions = models.PositiveIntegerField()
     active_time = models.IntegerField()
     passive_time = models.IntegerField()
-    ingredients = models.ManyToManyField(ingredients.models.Ingredient, through='UsesIngredient')
+    
+    ingredient_ingredients = models.ManyToManyField(ingredients.models.Ingredient, through='UsesIngredient')
     recipe_ingredients = models.ManyToManyField('self', through='UsesRecipe', symmetrical=False, related_name='r_ingredients')
     extra_info = models.TextField(default='')
     instructions = models.TextField()
@@ -90,6 +140,15 @@ class Recipe(models.Model):
     thumbnail = ImageSpecField([ResizeToFit(250, 250), AddBorder(2, 'Black')], image_field='image', format='PNG')
     
     accepted = models.BooleanField(default=False)
+    
+    # Make sure the 'ingredient' property is set! This should be a list containing all usesIngredients and usesRecipes
+    # of the recipe (including the recipes)
+    @property
+    def total_footprint(self):
+        total_footprint = 0
+        for uses in self.ingredients:
+            total_footprint += uses.ingredient.total_footprint
+        return total_footprint
     
 
 
@@ -102,7 +161,7 @@ class UsesIngredient(models.Model):
     ingredient = models.ForeignKey(ingredients.models.Ingredient, db_column='ingredient')
     
     group = models.CharField(max_length=100, blank=True)
-    amount = models.FloatField()
+    amount = models.FloatField(default=0)
     unit = models.ForeignKey(ingredients.models.Unit, db_column='unit')
     
     # TODO: Build in check that every instance of this model can only have units that the ingredient 
@@ -119,4 +178,7 @@ class UsesRecipe(models.Model):
     group = models.CharField(max_length=100, blank=True)
     portions = models.IntegerField()
     
-    # Build in check that when an instance of this is saved, no circular dependencies exist
+    def ingredient(self):
+        return self.recipe_used
+    
+    # TODO: Build in check that when an instance of this is saved, no circular dependencies exist
