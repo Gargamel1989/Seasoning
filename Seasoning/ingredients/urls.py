390cd9979f871624a3328dbd1dc24fb790f6993a
= <joep.driesen@seasoning.be>
2013-04-28 13:11:40 +0200

    Commit during disk rescue

--- Seasoning/ingredients/urls.py
+++ Seasoning/ingredients/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
     url(r'^(\d*)/$', 'ingredients.views.view_ingredient', name='ingredient'),
+    url(r'^add/$', 'ingredients.views.add_ingredient', name='add_ingredient'),
 )
--- Seasoning/ingredients/views.py
+++ Seasoning/ingredients/views.py
@@ -1,8 +1,22 @@
 
 from django.shortcuts import render
-from ingredients.models import Ingredient
+from ingredients.models import Ingredient, Synonym
+from ingredients.forms import IngredientForm
+from django.forms.models import inlineformset_factory
 
 def view_ingredient(request, ingredient_id):
     ingredient = Ingredient.objects.all_info(ingredient_id)
 
     return render(request, 'ingredients/view_ingredient.html', {'ingredient': ingredient})
+
+def add_ingredient(request):
+    
+    SynonymInlineFormSet = inlineformset_factory(Ingredient, Synonym, extra=0)
+    if request.method == 'POST':
+        pass
+    else:
+        form = IngredientForm()
+        synonym_formset = SynonymInlineFormSet()
+        
+        return render(request, 'ingredients/add_ingredient.html', {'form': form,
+                                                                   'synonym_formset': synonym_formset})
