319649a29b91ce026a1a5d34ce3e247045a78706
Gargamel1989 <joeper_100@hotmail.com>
2013-05-11 17:55:57 +0200

    db backup fix

--- Seasoning/Seasoning/settings.py
+++ Seasoning/Seasoning/settings.py
@@ -4,7 +4,6 @@ from dev_secrets import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
