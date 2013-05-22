864099d14c773306db28ff3c7c0229b9cc1de85b
= <joep.driesen@seasoning.be>
2013-05-22 16:16:49 +0200

    Urls portions

--- Seasoning/recipes/urls.py
+++ Seasoning/recipes/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^search/$', 'recipes.views.search_recipes', name='recipe_search'),
     url(r'^(\d*)/$', 'recipes.views.view_recipe', name='recipe_view'),
+    url(r'^(\d*)/portions/(\d*)/$', 'recipes.views.view_recipe'),
     url(r'^vote/(\d*)/(\d*)/$', 'recipes.views.vote'),
     url(r'^removevote/(\d*)/$', 'recipes.views.remove_vote'),
     
