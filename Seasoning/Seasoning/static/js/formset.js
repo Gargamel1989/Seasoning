624e0c4adbb39a6310b76ecb86dbd227b5bc9777
Gargamel1989 <joeper_100@hotmail.com>
2013-05-23 09:11:47 +0200

    formset.js is now 'generic' and works for recipes as wel

---
+++ Seasoning/Seasoning/static/js/formset.js
@@ -0,0 +1,41 @@
+function updateElementIndex(el, ndx) {
+	var id_regex = new RegExp('__prefix__');
+	var replacement = ndx;
+	el.find('*').each(function() {
+		if ($(this).attr("for")) $(this).attr("for", $(this).attr("for").replace(id_regex,
+				replacement));
+		if (this.id) this.id = this.id.replace(id_regex, replacement);
+		if (this.name) this.name = this.name.replace(id_regex, replacement);
+	});
+}
+
+function addForm(btn, prefix) {
+	var formCount = parseInt($('#id_' + prefix + '-TOTAL_FORMS').val());
+	
+	var empty_row = $('.empty.' + prefix + '-form');
+	var new_row = $(empty_row).clone(false).removeClass('empty');
+	updateElementIndex(new_row, formCount)
+	
+	new_row.insertBefore(empty_row);
+	$('#id_' + prefix + '-TOTAL_FORMS').val(formCount + 1);
+		
+	return false;
+}
+
+function ok() {
+	$('#synonym_set-addbtn').click(function() {
+		return addForm(this, "synonym_set");
+	});
+
+	$('#can_use_unit-addbtn').click(function() {
+		return addForm(this, "can_use_unit");
+	});
+
+	$('#available_in_country-addbtn').click(function() {
+		return addForm(this, "available_in_country");
+	});
+
+	$('#uses-addbtn').click(function() {
+		return addForm(this, "uses");
+	});
+}
