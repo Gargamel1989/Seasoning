46e12f45175a4b8378a886b90f9e5b1605e8387c
Joep <Joep@192.168.0.212>
2013-07-28 17:59:40 +0200

    layout fixes + only 1 namechange allows

--- Seasoning/general/templatetags/home_extras.py
+++ Seasoning/general/templatetags/home_extras.py
@@ -23,7 +23,8 @@ import re
 register = template.Library()
 
 paths = {'RECEPTEN': ['/recipes/.*'],
-         'INFORMATIE': ['/ingredients/', '/ingredients/\d*/', '/information/'],
+         'INFORMATIE': ['/ingredients/', '/ingredients/\d*/', '/information/',
+                        '/news/.*', '/about/'],
          'ADMIN': ['/ingredients/list/', '/ingredients/add/', '/ingredients/edit/(\d*)/', 
                    '/ingredients/units/.*', '/authentication/users/','/faq/admin/.*',
                    '/admin/.*'],
