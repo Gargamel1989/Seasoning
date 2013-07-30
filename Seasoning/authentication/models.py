56e09f022cab914a2c3f3c99aca63652cc457f2d
unknown <u0087393@.luna.kuleuven.be>
2013-07-30 14:56:14 +0200

    tweaks + formset bugfix

--- Seasoning/authentication/models.py
+++ Seasoning/authentication/models.py
@@ -205,8 +205,6 @@ class User(models.Model):
             else:
                 self.name_changed = True
         super(User, self).save(*args, **kwargs);
-        print self.__cached_givenname__
-        print self.__cached_surname__
         self.__cached_givenname__ = self.givenname
         self.__cached_surname__ = self.surname
     
