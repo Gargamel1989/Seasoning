0f98198183050a08d17e92ba12edb0710498e656
Joep <Joep@192.168.1.110>
2013-10-26 02:42:00 +0200

    news mooier gemaakt

--- Seasoning/news/views.py
+++ Seasoning/news/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render
 def news(request):
     news_list = NewsItem.objects.filter(visible=True).order_by('time_published')
     
-    # Split the result by 12
-    paginator = Paginator(news_list, 12)
+    # Split the result by 3
+    paginator = Paginator(news_list, 3)
     
     page = request.GET.get('page')
     try:
@@ -14,9 +14,10 @@ def news(request):
     except PageNotAnInteger:
         news = paginator.page(1)
     except EmptyPage:
-        news = paginator.page(paginator.num_pages)
+        # Raise error because no more news is availble
+        raise 
     
-    if request.method == 'POST' and request.is_ajax():
+    if request.is_ajax():
         return render(request, 'includes/news_list.html', {'news': news})
         
     return render(request, 'news/view_news.html', {'news': news})
